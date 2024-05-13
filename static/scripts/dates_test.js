7bb3066e0185dea48e2507d2f018fe77f6e7f93f
PabloTsvetkov <pcvetkov53@gmail.com>
2024-05-14 00:43:31 +0300

    range of questions + repeat test without styles

--- static/scripts/dates_test.js
+++ static/scripts/dates_test.js
@@ -1,5 +1,5 @@
 // Загрузка вопросов и ответов из JSON-файла
-const coutOfQuestions = 10; // сколько вопросов отображается
+let countOfQuestions = 1; // сколько вопросов отображается
 fetch("/static/tests_jsons/dates_test.json")
     .then(response => response.json())
     .then(data => {
@@ -8,6 +8,39 @@ fetch("/static/tests_jsons/dates_test.json")
         let correctAnswers = 0;
         let helpUsedIndArr = [];
         let QuestionNow = 0;
+
+        // Обработка количества вопросов
+        document.getElementById("question-count-slider").max = questions.length;
+        document.getElementById("question-container").style.display = "none";
+        const questionCountSlider = document.getElementById('question-count-slider');
+        const questionCountDisplay = document.getElementById('question-count-display');
+        const startButton = document.getElementById('start-button');
+
+        questionCountSlider.addEventListener('input', () => {
+            const count = questionCountSlider.value;
+            questionCountDisplay.textContent = count;
+            countOfQuestions = parseInt(count);
+            helpUsedIndArr = [];
+        });
+
+        startButton.addEventListener('click', () => {
+            document.getElementById('question-selection-container').style.display = 'none';
+            document.getElementById('question-container').style.display = "flex"; 
+            displayCurrentQuestion();
+        });
+        // Пройти заново
+        const restartButton = document.getElementById('restart-button');
+
+        restartButton.addEventListener('click', () => {
+            currentQuestionIndex = 0;
+            correctAnswers = 0;
+            helpUsedIndArr = [];
+            QuestionNow = 0;
+            document.getElementById('results-container').style.display = 'none';
+            document.getElementById('question-selection-container').style.display = 'block';
+            document.getElementById('question-container').style.display = 'none';
+        });
+
 
         // Функция для отображения текущего вопроса
         function displayCurrentQuestion() {
@@ -19,7 +52,6 @@ fetch("/static/tests_jsons/dates_test.json")
             }
 
             let rnd = Math.floor(Math.random() * helpUnusedIndArr.length);
-            console.log(helpUnusedIndArr);
             helpUsedIndArr.push(helpUnusedIndArr[rnd]);
             QuestionNow = helpUnusedIndArr[rnd];
 
@@ -41,7 +73,7 @@ fetch("/static/tests_jsons/dates_test.json")
             progressBar.classList.add('progressBarOuter')
             const progress = document.createElement('div');
             progress.classList.add('progressBarInner');
-            progress.style.width = (100 / coutOfQuestions * currentQuestionIndex) + '%';
+            progress.style.width = (100 / countOfQuestions * currentQuestionIndex) + '%';
             progressBar.appendChild(progress);
 
             numberQuestionContainer.appendChild(progressBar);
@@ -78,24 +110,42 @@ fetch("/static/tests_jsons/dates_test.json")
             }
 
             currentQuestionIndex++;
-            if (currentQuestionIndex < coutOfQuestions) {
+            if (currentQuestionIndex < countOfQuestions) {
                 displayCurrentQuestion();
             } else {
                 showFinalResults();
             }
         }
 
-        // Функция для отображения результатов теста
+        // отображения финальных результатов
         function showFinalResults() {
             const questionContainer = document.getElementById('question-container');
             questionContainer.innerHTML = '';
 
-
             const resultsElement = document.createElement('div');
             resultsElement.classList.add('results');
-            resultsElement.textContent = `Ваш результат: ${correctAnswers} / ${coutOfQuestions}`;
+            resultsElement.textContent = `Ваш результат: ${correctAnswers} / ${countOfQuestions}`;
             questionContainer.appendChild(resultsElement);
+
+            // const restartButton = document.createElement('button');
+            // restartButton.id = 'restart-button';
+            // restartButton.textContent = 'Пройти заново';
+            // questionContainer.appendChild(restartButton);
+
+            restartButton.addEventListener('click', () => {
+                currentQuestionIndex = 0;
+                correctAnswers = 0;
+                helpUsedIndArr = [];
+                QuestionNow = 0;
+                document.getElementById('results-container').style.display = 'none';
+                document.getElementById('question-selection-container').style.display = 'block';
+                document.getElementById('question-container').style.display = 'none';
+            });
+
+            document.getElementById('question-selection-container').style.display = 'none';
+            document.getElementById('results-container').style.display = 'block';
         }
+
 
         // Отображение первого вопроса
         displayCurrentQuestion();
